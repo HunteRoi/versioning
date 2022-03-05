780efec1ffb0e9d05222d767e15f934d3026f14e
HunteRoi <me@tinaeldevresse.eu>
2022-03-05 18:09:55 +0100

    docs: add coverage to github page

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-	preset: "ts-jest",
-	testEnvironment: "node",
-	verbose: true,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  verbose: true,
+  coverageDirectory: 'docs/coverage',
 };
