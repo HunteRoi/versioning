fd9bb03c5236602fc3e4548c89868cdd885bef04
HunteRoi <me@tinaeldevresse.eu>
2022-03-05 16:47:03 +0100

    TCR

--- src/VersionManager.ts
+++ src/VersionManager.ts
@@ -8,13 +8,23 @@ import { IVersion } from 'IVersion';
  * @implements {IVersionManager}
  */
 export class VersionManager implements IVersionManager {
+	/**
+	 * The managed version.
+	 *
+	 * @private
+	 */
 	private readonly _version: IVersion;
 
+	/**
+	 * Creates an instance of {@link VersionManager}.
+	 * @param version the version to manage
+	 */
 	constructor(version: IVersion) {
 		if (!version) throw new Error("The 'version' parameter is mandatory!");
 		this._version = version;
 	}
 
+	/** @inheritdoc */
 	update(type: Type, preRelease?: string, build?: string): void {
 		if (type === 'major') this._version.incrementMajor();
 		if (type === 'minor') this._version.incrementMinor();
