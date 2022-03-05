f4fba3ce2743adae3e1147c8ad1c96a4d09e66ad
HunteRoi <me@tinaeldevresse.eu>
2022-03-05 03:02:19 +0100

    TCR

--- src/Version.ts
+++ src/Version.ts
@@ -1,13 +1,13 @@
 import { IVersion } from './IVersion';
 
 export class Version implements IVersion {
-	private _major: number;
-	private _minor: number;
-	private _patch: number;
+	major: number;
+	minor: number;
+	patch: number;
 
 	constructor() {
-		this._major = 1;
-		this._minor = 0;
-		this._patch = 0;
+		this.major = 1;
+		this.minor = 0;
+		this.patch = 0;
 	}
 }
